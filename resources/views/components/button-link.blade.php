de11c579fcbf6854f204d6b9ab9ab348e2cb5cfd
habanerocity <lindyo87@gmail.com>
2025-03-13 05:27:16 +0800

    added js for hamburger menu toggle

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,7 +1,14 @@
-@props(['url' => '/', 'icon' => null, 'bgClass' => 'bg-yellow-500', 'hoverClass' => 'hover:bg-yellow-600', 'textClass' => 'text-black'])
+@props([
+'url' => '/', 
+'icon' => null, 
+'bgClass' => 'bg-yellow-500', 
+'hoverClass' => 'hover:bg-yellow-600', 
+'textClass' => 'text-black',
+'block' => false
+])
 
 <a href="{{ $url }}" >
-    <button class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded transition duration-300" >
+    <button class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded transition duration-300 {{ $block ? 'block' : '' }}" >
         @if($icon)
         <i class="fa fa-{{ $icon }} mr-1"></i>
         @endif
