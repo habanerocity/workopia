dee45b8fe050fc7f6dbedf23ca29e809f0d84577
habanerocity <lindyo87@gmail.com>
2025-03-17 23:14:47 +0800

    added new fields to job_listings table via new migration file

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -30,5 +31,12 @@ class Job extends Model
         'company_description',
         'company_logo',
         'company_website',
+        'user_id',
     ];
+
+    // Relation to user
+    public function user(): BelongsTo   
+    {
+        return $this->belongsTo(User::class);
+    }
 }
