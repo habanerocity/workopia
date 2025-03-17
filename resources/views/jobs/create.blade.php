3df90dc9ac4a0743331929a707b4cc1b4cb38895
habanerocity <lindyo87@gmail.com>
2025-03-17 22:54:48 +0800

    job schema update migration

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,10 +1,20 @@
 <x-layout>
     <x-slot name="title">Create Job </x-slot>
     <h1>Create New Job</h1>
-    <form action="/jobs" method="POSt">
+    <form action="/jobs" method="POST">
         @csrf
-        <input type="text" name="title" placeholder="Job Title">
-        <input type="text" name="description" placeholder="Job Description">
+        <div class="my-5">
+            <input class="px-4 py-2 bg-white border rounded focus:outline-none" type="text" name="title" placeholder="Job Title" value="{{ old('title') }}">
+            @error('title')
+                <div class="text-red-500 mt-2 text-sm">{{$message}}</div>
+            @enderror
+        </div>
+        <div class="my-5">
+            <input class="px-4 py-2 bg-white border rounded focus:outline-none" type="text" name="description" placeholder="Job Description" value="{{ old('description') }}">
+            @error('description')
+                <div class="text-red-500 mt-2 text-sm">{{$message}}</div>
+            @enderror
+        </div>
         <button type="submit">Create Job</button>
     </form>
 </x-layout>
