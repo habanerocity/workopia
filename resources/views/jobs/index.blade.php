feee5005e198b9b62cfe3547d3486b8d5da8ad60
habanerocity <lindyo87@gmail.com>
2025-03-19 00:25:39 +0800

    added dynamic job cards to all jobs page

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,10 +1,13 @@
 <x-layout>
-    <h1>Available Jobs</h1>
-    <ul>
-        @forelse($jobs as $job)
-            <li><a href="{{ route('jobs.show', $job->id) }}">{{$job->title}}</a> - {{$job->description}}</li>
-        @empty
-            <li>No jobs available</li>
-        @endforelse
-    </ul>
+    <h1 class="text-2xl">All Jobs</h1>
+    
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+
+    @forelse($jobs as $job)
+        <x-job-card :job="$job" />
+    @empty
+    <p>No jobs available</p>
+    @endforelse
+    </div>
+    
 </x-layout>
