2995b9efac1dd8dda9a09bccccdf863166db4df3
habanerocity <lindyo87@gmail.com>
2025-03-21 11:52:10 +0800

    implemented job listing delete functionality

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -17,7 +17,9 @@
                             >Edit</a
                         >
                         <!-- Delete Form -->
-                        <form method="POST">
+                        <form method="POST" action="{{ route('jobs.destroy', $job->id) }}" onSubmit="return confirm('Are you sure you want to delete this joblisting?')">
+                            @csrf
+                            @method('DELETE')
                             <button
                                 type="submit"
                                 class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded"
