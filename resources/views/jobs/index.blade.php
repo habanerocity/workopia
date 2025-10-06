0a0ec9fba1a22b16924346f7b5a57dca7757bf1e
habanerocity <lindyo87@gmail.com>
2025-10-07 00:05:55 +0800

    added search functionality via the query builder

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,6 +2,12 @@
     <div class="bg-blue-900 h-24 px-4 mb-4 flex justify-center items-center rounded">
         <x-search />
     </div>
+
+    {{-- Back Button  --}}
+    @if(request()->has('keywords') || request()->has('location'))
+        <a href="{{  route('jobs.index') }}" class="hover:bg-gray-600 bg-gray-700 text-white px-4 py-2 rounded mb-4 inline-block"><i class="fa fa-arrow-left mr-1"></i>Back to all jobs</a>
+    @endif
+
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
     @forelse($jobs as $job)
         <x-job-card :job="$job" />
