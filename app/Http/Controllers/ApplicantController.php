768e23b1637cd2077aa6545066d51060614ee46c
habanerocity <lindyo87@gmail.com>
2025-10-09 07:19:55 +0800

    added email functionality with mailer mailtrap.io

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Job;
 use App\Models\Applicant;
-use Illuminate\Http\RedirectResponse;
+use App\Mail\JobApplied;
 
 class ApplicantController extends Controller
 {
@@ -39,6 +41,9 @@ class ApplicantController extends Controller
         $application->job_id = $job->id;
         $application->user_id = auth()->id();
         $application->save();
+
+        // Send notification email to the job owner
+        Mail::to($job->user->email)->send(new JobApplied());
 
         return redirect()->back()->with('success', 'Your application has been submitted!');
     }
---
+++ resources/views/emails/job-applied.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Workopia Job Application</title>
+</head>
+<body>
+    <p>There has been a new application for your Workopia job posting.</p>
+    <p>Login to your Workopia account to view the application.</p>
+</body>
+</html>
